7fcfa07d937730efc302d7964c78cb014e566604
JT Cho <jtcho123@gmail.com>
2024-01-06 17:51:03 -0500

    SueChedule begin jan 6 2024

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header({}) {
+  return (
+    <header>
+      <div>
+        {/* put logo and link || router */}
+        <a href="">
+          <img src="" alt="" />
+        </a>
+      </div>
+    </header>
+  );
+}
