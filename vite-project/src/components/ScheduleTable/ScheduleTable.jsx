7194c26791f5165543893bb672762c35b68cfe92
JT Cho <jtcho123@gmail.com>
2024-01-13 23:45:05 -0500

    update minor design

--- vite-project/src/components/ScheduleTable/ScheduleTable.jsx
+++ vite-project/src/components/ScheduleTable/ScheduleTable.jsx
@@ -1,16 +1,20 @@
 export default function ScheduleTable() {
-  let style = "h-8 w-16 bg-orange-500 m-auto p-8";
+  let style =
+    "h-full w-full bg-orange-500 m-auto p-8 transition ease-in-out hover:ease-in hover:col-span-2";
+
+  let verticalTransitionStyle =
+    "min-h-4064 h-full w-full bg-orange-500 m-auto p-8 transition ease-in-out hover:ease-in hover:row-span-2";
 
   return (
     <section>
-      <div className="grid grid-cols-4 gap-8">
+      <div className="grid sm:grid-cols-4 gap-8 text-center h-96">
         <div className={style}>01</div>
-        <div>02</div>
-        <div>03</div>
-        <div>04</div>
-        <div>05</div>
-        <div>06</div>
-        <div>07</div>
+        <div className={style}>02</div>
+        <div className={style}>03</div>
+        <div className={verticalTransitionStyle}>04</div>
+        <div className={style}>05</div>
+        <div className={style}>06</div>
+        <div className={style}>07</div>
       </div>
     </section>
   );
